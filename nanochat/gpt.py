3a913536bb1ddde92b843c2ebef8fe54a9c71e8a
Hamidreza Alikhani <hamidra@drg-gpu.ics.uci.edu>
2025-11-10 19:17:17 -0800

    roPE for gpt.py

--- nanochat/gpt.py
+++ nanochat/gpt.py
@@ -1,1 +1,52 @@
-#TODO
+"""
+GPT model 
+Notable features:
+- rotary embeddings (and no positional embeddings)
+- QK norm
+- untied weights for token embedding and lm_head
+- relu^2 activation in MLP
+- norm after token embedding
+- no learnable params in rmsnorm
+- no bias in linear layers
+- Multi-Query Attention (MQA) support for more efficient inference
+"""
+
+import math
+from functools import partial
+from dataclasses import dataclass
+
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F
+
+from nanochat.common import get_dist_info, print0
+from nanochat.muon import Muon, DistMuon
+from nanochat.adamw import DistAdamW
+
+@dataclass
+class GPTConfig:
+    sequence_len: int = 1024
+    vocab_size: int = 50304
+    n_layer: int = 12
+    n_head: int = 6      # for query heads
+    n_kv_head: int = 6   # for key/value heads (MQA)
+    n_embd: int = 768
+
+def norm(x):
+    # Purely functional rmsnorm with no learnable params
+    return F.rms_norm(x, (x.size(-1),))
+
+def apply_rotary_emb(x, cos, sin):
+    """Applies RoPE by rotating pairs of channels in the last dim."""
+    assert x.ndim == 4  # multihead attention, (B, T, H, D)
+    d = x.shape[3]//2
+    x1, x2 = x[..., :d], x[..., d:] 
+    y1 = x1 * cos + x2 * sin # rotate pairs of dims
+    y2 = x1 * (-sin) + x2 * cos
+    out = torch.cat([y1, y2], 3) # re-assemble
+    out = out.to(x.dtype) # ensure input/output dtypes match
+    return out
+
+
+
+
