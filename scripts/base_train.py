8f512d1dd013ab393fa7a3239d695a260ed4ce35
Hamidreza Alikhani <hamidra@drg-gpu.ics.uci.edu>
2025-11-19 10:21:53 -0800

    modified dataloader, first part of base_train.py implemented

---
+++ scripts/base_train.py
@@ -0,0 +1,71 @@
+"""
+Train model. Run as:
+
+python base_train.py
+
+or distributed as:
+
+torchrun --nproc_per_node=8 base_train.py
+
+If you are only on CPU/Macbook, you'll want to train a much much smaller LLM. Example:
+python -m scripts.base_train --depth=4 --max_seq_len=512 --device_batch_size=1 --eval_tokens=512 --core_metric_every=-1 --total_batch_size=512 --num_iterations=20
+"""
+
+import os
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+import time
+from contextlib import nullcontext
+
+import wandb
+import torch
+
+from nanochat.gpt import GPT, GPTConfig
+from nanochat.dataloader import tokenizing_distributed_data_loader, tokenizing_distributed_data_loader_with_state
+from nanochat.common import compute_init, compute_cleanup, print0, DummyWandb, print_banner, get_base_dir, autodetect_device_type
+from nanochat.tokenizer import get_tokenizer, get_token_bytes
+from nanochat.checkpoint_manager import save_checkpoint, load_checkpoint
+from nanochat.loss_eval import evaluate_bpb
+from nanochat.engine import Engine
+from scripts.base_eval import evaluate_model
+print_banner()
+
+# -----------------------------------------------------------------------------
+# User settings
+run = "dummy" # wandb run name default ("dummy" is special - we won't log to wandb)
+# Runtime
+device_type = "" # cuda|cpu|mps (empty => autodetect good device type default, in order: CUDA > MPS > CPU)
+# Model architecture
+depth = 20 # the depth of the Transformer model to train, rest of the kwargs are derived
+max_seq_len = 2048 # max context length
+# Training horizon. Only one of these 3 will be used, in this order of precedence.
+num_iterations = -1 # explicit number of steps of the optimization (-1 = disable)
+target_flops = -1.0 # calculate num_iterations to reach target_flops. Useful for scaling laws experiments (-1 = disable)
+target_param_data_ratio = 20 # calculate num_iterations to maintain fixed data:param ratio (Chinchilla=20) (-1 = disable)
+# Optimization
+device_batch_size = 32 # per-device batch size (set to not OOM)
+total_batch_size = 524288 # total desired batch size, in #tokens
+embedding_lr = 0.2 # learning rate for the embedding parameters (Adam)
+unembedding_lr = 0.004 # learning rate for the unembedding parameters (Adam)
+weight_decay = 0.0 # weight decay for the embedding/unembedding parameters (Adam)
+matrix_lr = 0.02 # learning rate for the matrix parameters (Muon)
+grad_clip = 1.0 # gradient clipping value (0.0 = disabled)
+warmup_ratio = 0.0 # ratio of iterations for LR warmup
+warmdown_ratio = 0.2 # ratio of iterations for LR warmdown
+final_lr_frac = 0.0 # final LR is this fraction of the initial LR
+resume_from_step = -1 # resume training from this step of the optimization (-1 = disable)
+# Evaluation
+eval_every = 250 # every how many steps to evaluate the model for val bpb
+eval_tokens = 20*524288 # number of tokens to evaluate val loss on
+core_metric_every = 2000 # every how many steps to evaluate the core metric (-1 = disable)
+core_metric_max_per_task = 500 # examples per task in estimating the core metric
+sample_every = 2000 # every how many steps to sample from the model
+save_every = -1 # every how many steps to save model checkpoints (-1 = disable, and save only at the end of the run)
+# Output
+model_tag = "" # optionally override the model tag for the output checkpoint directory name
+# now allow CLI to override the settings via the configurator 
+config_keys = [k for k, v in globals().items() if not k.startswith("_") and isinstance(v, (int, float, str, bool))]
+# Execute configurator.py, which reads CLI flags and/or config files and overrides these globals.
+exec(open(os.path.join('nanochat', 'configurator.py')).read())
+user_config = {k: globals()[k] for k in config_keys}  # will be useful for logging
+# -----------------------------------------------------------------------------
+
