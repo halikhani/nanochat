334b4665c3d94656bd374bdacefa242ad2c2df2d
Hamidreza Alikhani <ha.alikhani.k@gmail.com>
2025-12-30 11:55:39 -0800

    mid_train init commit

---
+++ scripts/mid_train.py
@@ -0,0 +1,11 @@
+"""
+Midtrain the model. Same as pretraining but simpler.
+Run as:
+
+python -m scripts.mid_train
+
+Or torchrun for training:
+
+torchrun --standalone --nproc_per_node=8 -m scripts.mid_train -- --device_batch_size=16
+"""
+
