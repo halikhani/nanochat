74e13a90e0d8842aa44e6d1eca60df5fe7acc174
Hamidreza Alikhani <ha.alikhani.k@gmail.com>
2025-12-30 21:25:20 -0800

    init mid_train, back to tasks implementation

--- scripts/mid_train.py
+++ scripts/mid_train.py
@@ -9,3 +9,26 @@ Or torchrun for training:
 torchrun --standalone --nproc_per_node=8 -m scripts.mid_train -- --device_batch_size=16
 """
 
+from collections import deque
+import os
+os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
+import time
+import wandb
+import torch
+from contextlib import nullcontext
+from nanochat.common import compute_init, compute_cleanup, print0, DummyWandb, get_base_dir, autodetect_device_type
+from nanochat.tokenizer import get_token_bytes
+from nanochat.checkpoint_manager import save_checkpoint, load_model
+from nanochat.loss_eval import evaluate_bpb
+import torch.distributed as dist
+
+# Task dataset classes for training mixture
+from tasks.common import TasMixture
+from tasks.gsm8k import GSM8K
+from tasks.mmlu import MMLU
+from tasks.smoltalk import SmolTalk
+from tasks.customjson import CustomJSON
+from tasks.spellingbee import SimpleSpelling, SpellingBee
+
+
+
